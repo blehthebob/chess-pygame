324da813232060613282abda2f60fd064705edfd
blehthebob <ruiqi.zhang@gmail.com>
2023-08-23 23:19:14 +0100

    Add board and piece implementation structure

---
+++ board.py
@@ -0,0 +1,8 @@
+from pieces import *
+
+class ChessBoard:
+    def __init__(self):
+        self.board = [[None] * 8] * 8
+
+    def load_square():
+        pass
---
+++ main.py
@@ -0,0 +1,7 @@
+from board import *
+
+def main():
+    myboard = ChessBoard()
+
+if __name__ == "__main__":
+    main()
